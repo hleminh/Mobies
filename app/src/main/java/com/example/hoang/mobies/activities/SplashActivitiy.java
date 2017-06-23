d94c6bc6d313246f6359924e88359342ee07fc87
Le Minh Hoang <le.minhhoang@outlook.com>
2017-06-23 11:53:48 +0700

    Added animated splash and fixed some layout bugs.

--- app/src/main/java/com/example/hoang/mobies/activities/SplashActivitiy.java
+++ app/src/main/java/com/example/hoang/mobies/activities/SplashActivitiy.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
@@ -16,18 +18,20 @@ import butterknife.ButterKnife;
 
 public class SplashActivitiy extends AppCompatActivity {
     @BindView(R.id.iv_logo)
-    ImageView ivLogo;
+    TextView tvLogo;
     @BindView(R.id.tv_splash)
     TextView tvSplash;
+    @BindView(R.id.pb_splash)
+    ProgressBar pbSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
-        YoYo.with(Techniques.FadeIn).duration(1500).playOn(ivLogo);
-        YoYo.with(Techniques.FadeIn).duration(1500).playOn(tvSplash);
-        CountDownTimer countDownTimer = new CountDownTimer(3000,1000) {
+        YoYo.with(Techniques.FadeIn).duration(1000).playOn(tvLogo);
+        YoYo.with(Techniques.FadeIn).duration(1000).playOn(tvSplash);
+        CountDownTimer countDownTimer = new CountDownTimer(5000,1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
@@ -35,6 +39,7 @@ public class SplashActivitiy extends AppCompatActivity {
 
             @Override
             public void onFinish() {
+                pbSplash.setVisibility(View.INVISIBLE);
                 Intent intent = new Intent(SplashActivitiy.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK );
                 startActivity(intent);
