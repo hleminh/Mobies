330502a43aeedf6134761de8714c86fb547d277a
Le Minh Hoang <le.minhhoang@outlook.com>
2017-06-28 03:19:54 +0700

    Forgot toolbar lul.

--- app/src/main/java/com/example/hoang/mobies/fragments/NewsDetailFragment.java
+++ app/src/main/java/com/example/hoang/mobies/fragments/NewsDetailFragment.java
@@ -3,16 +3,23 @@ package com.example.hoang.mobies.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hoang.mobies.R;
+import com.example.hoang.mobies.managers.ScreenManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewsDetailFragment extends Fragment {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
 
     public NewsDetailFragment() {
@@ -24,7 +31,17 @@ public class NewsDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_news_detail, container, false);
+        ButterKnife.bind(this, view);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setTitle(R.string.news_detail);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ScreenManager.backFragment(getFragmentManager());
+            }
+        });
+        return view;
     }
 
 }
